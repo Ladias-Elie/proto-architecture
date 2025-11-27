df46f57575583c64bbd6e8bbc46f407a234d20fa
Elie Ladias <elie@Elies-MacBook-Pro.local>
2025-11-27 15:12:48 +0100

    to

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/proto-architecture/",  // <-- 🔥 obligatoire pour GitHub Pages
+  base: "/",  // <-- 🔥 obligatoire pour GitHub Pages
   server: {
     host: "::",
     port: 8080,

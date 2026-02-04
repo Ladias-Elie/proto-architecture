412676cda02e882e7e9e9f8e562f411885466ace
elie <elie@MacBook-Air-de-elie.local>
2026-02-04 13:41:03 +0100

    sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -15,6 +15,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 1.0,
     },
     {
+      url: `${baseUrl}/architecte-renovation-lyon`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.9,
+    },
+    {
       url: `${baseUrl}/projets`,
       lastModified: new Date(),
       changeFrequency: 'weekly',
